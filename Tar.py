a59e097cc2ec7cf357022a17f1639e4bade38e12
RG_WorkSpace <WorkSpace@MacBook-Pro-de-Romain.local>
2013-05-02 09:57:07 +0200

    Constructeur + open

--- Tar.py
+++ Tar.py
@@ -3,21 +3,23 @@ class Tar:
     
     #----------------------------------------------------------------------
     #Class Tar contenant les methode de manipulation des fichiers tar
-    #
     #----------------------------------------------------------------------
     
     def __init__(self, _Param):
         """Constructor"""
-        FileName = _Param
-        print(FileName)
+        self.FileName = _Param
+        print(self.FileName)
+        self.open_tar()
         
     def open_tar(self):
         # Ouvre le fichier passer dans le constructeur
-        print("TODO")
+        self.f = open(self.FileName,"r")
+        data = self.f.readline()
+        print data 
         
     def close_tar(self):
         # Ferme l'archive.
-        print("TODO")
+        self.f.close()
 
     def chdir_tar(self, path):
         # Changement de repertoire.
--- main.py
+++ main.py
@@ -12,44 +12,5 @@ from TarFile import TarFile
 print("Start : ")
 
 
-
-mon_fichier = open("TestFile.tar", "rb")                    #Ouverture du fichier en mode Binaire
-data = mon_fichier.read()                                   #Recuperation des données en mode normal
-mon_fichier.close()                                         #Fermeture des fichiers
-
-print("Lecture de data : ")                                 #Affichage
-print data                                                  #---------
-
-print("\nLecture en hÈxadecimal ")
-data_hex = data.encode("hex")                               #Copy des donnée en hexadecimal 
-print data_hex                                              #Affichage
-
-TarFileName = data_hex[0:200]                               #Recuperation Des informations en fonction de la structure de l'entete tar
-tarFileType = data_hex[2360:2362]
-
-print(TarFileName)
-print(tarFileType)
-
-IsDir = "5"                                                 #Test Pseudo Enumeration
-IsDir = IsDir.encode("hex")
-IsFile = "0"
-IsFile = IsFile.encode("hex")
-
-
-
-if tarFileType == IsFile :                                  #Test du typage de fichier
-    print("{} Is a File".format(data[:10]))
-elif tarFileType == IsDir:
-    print("{} Is a directory" .format(data[:10]))
-else:
-    print("Unknow")
-
-
-print("Taille de larchive: ")                               #Affichage de la taille total du fichier 
-print(len(data_hex))
-
-#SplitTest = data.split()
-#print(SplitTest[1])
-
-# Fin des test RomainG
-
+#Creation d'un nouvelle objet tar
+Archive = Tar("TestFile.tar")
