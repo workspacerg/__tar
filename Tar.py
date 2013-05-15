4a4c55bc7d8b3706cc05bc324ad77e5f0917088e
Romain Gabel <workspace.rg@gmail.com>
2013-05-15 19:41:55 +0200

    AddParseFile  v0.1

--- Tar.py
+++ Tar.py
@@ -13,9 +13,7 @@ class Tar:
         
     def open_tar(self):
         # Ouvre le fichier passer dans le constructeur
-        self.f = open(self.FileName,"r")
-        data = self.f.readline()
-        print data 
+        self.f = open(self.FileName,"rb")
         
     def close_tar(self):
         # Ferme l'archive.
@@ -32,6 +30,95 @@ class Tar:
     def opendir_tar(self):
         # retourne un objet de la classe tardir
         print("TODO")
+
+    def isFileOrDir(self, _FileType):
+        print "TestFileOrDir : " + _FileType
+        
+        IsDir = "5"                                                 #Test Pseudo Enumeration
+        IsFile = "0"
+                
+        if _FileType == IsFile :                                  #Test du typage de fichier
+            print("Is a File")
+        elif _FileType == IsDir:
+            print("Is a directory")
+        else:
+            print("Unknow")        
+        
+    
+    def parseTar(self):
+        print "Debut du parsage complet : \n" 
+        data = self.f.read()
+        currentLoc = 0
+        
+        TarFileName = data[currentLoc:currentLoc+100]
+        print "Nom :" + TarFileName
+        TarFileSize = str(int(data[currentLoc+124:currentLoc+136], 8))
+        print "Size: " + TarFileSize
+        TarFileType = data[currentLoc+156:currentLoc+157]
+        print "Type is :" + TarFileType
+        self.isFileOrDir(TarFileType)
+        
+        currentLoc += 512
+        
+        TarFileName = data[currentLoc:currentLoc+100]
+        print "Nom :" + TarFileName
+        TarFileSize = str(int(data[currentLoc+124:currentLoc+136], 8))
+        print "Size: " + TarFileSize
+        TarFileType = data[currentLoc+156:currentLoc+157]
+        print "Type is :" + TarFileType
+        self.isFileOrDir(TarFileType)    
+        
+        currentLoc += 512
+        
+        TarFileName = data[currentLoc:currentLoc+100]
+        print "Nom :" + TarFileName
+        TarFileSize = str(int(data[currentLoc+124:currentLoc+136], 8))
+        print "Size: " + TarFileSize
+        TarFileType = data[currentLoc+156:currentLoc+157]
+        print "Type is :" + TarFileType
+        self.isFileOrDir(TarFileType)    
+        
+        currentLoc += 512
+        
+        #Lecture de fichier
+        
+        TarFileContent = data[currentLoc:currentLoc+int(TarFileSize)]
+        print "Content : " + TarFileContent
+        save = 0
+        while save < int(TarFileSize,10) :
+            currentLoc += 512
+            save += 512
+            
+        TarFileName = data[currentLoc:currentLoc+100]
+        print "Nom :" + TarFileName
+        TarFileSize = str(int(data[currentLoc+124:currentLoc+136], 8))
+        print "Size: " + TarFileSize
+        TarFileType = data[currentLoc+156:currentLoc+157]
+        print "Type is :" + TarFileType
+        self.isFileOrDir(TarFileType)          
+            
+        currentLoc += 512
+                
+                #Lecture de fichier
+                
+        TarFileContent = data[currentLoc:currentLoc+int(TarFileSize)]
+        print "Content : " + TarFileContent
+        save = 0
+        while save < int(TarFileSize,10) :
+            currentLoc += 512
+            save += 512 
+        
+        TarFileName = data[currentLoc:currentLoc+100]
+        print "Nom :" + TarFileName
+        TarFileSize = str(int(data[currentLoc+124:currentLoc+136], 8))
+        print "Size: " + TarFileSize
+        TarFileType = data[currentLoc+156:currentLoc+157]
+        print "Type is :" + TarFileType
+        self.isFileOrDir(TarFileType)            
+        
+        
+        print("Fin du parsage complet\n")
+        
 
     
 ######################################################################## 
--- main.py
+++ main.py
@@ -11,6 +11,7 @@ from TarFile import TarFile
 
 print("Start : ")
 
-
 #Creation d'un nouvelle objet tar
 Archive = Tar("TestFile.tar")
+
+Archive.parseTar();
