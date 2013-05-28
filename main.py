ddb7ded3673306f14a166c39901baafbb372d95e
Romain Gabel <workspace.rg@gmail.com>
2013-05-28 08:47:23 +0200

    Del Com

--- main.py
+++ main.py
@@ -2,12 +2,6 @@ from Tar import Tar
 from TarDir import TarDir
 from TarFile import TarFile
 
-    
-###########################################################################
-# Debut des test RomainG
-# Ca peut servir pour voir vite fais coment ca marche, mais nllement du code final xD
-# Je me en forme mercredi dans les bons fichier.
-###########################################################################
 
 print("Start : ")
 Recup = ["",""]
